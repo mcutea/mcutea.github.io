ac2ac94acd25c1190f4fb770e485332841cc35b1
Henry Wu <its@henrywu.tw>
2020-05-15 20:21:53 +0800

    Update insert.js

--- inc/insert.js
+++ inc/insert.js
@@ -1,1 +1,8 @@
 console.log('Hello!');
+imgList = {
+  "T01": "https://i.imgur.com/Byr01yh.jpg"
+}
+eqipTable = document.querySelectorAll('span[name=table1] tr');
+for(i = 2; i <= eqipTable.length; i++) {
+  console.log(imgList[document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim()]);
+}
