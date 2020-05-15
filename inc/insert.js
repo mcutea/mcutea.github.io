b2b466fefe4fbf782eeff29430d90bcc8d196a1a
Henry Wu <its@henrywu.tw>
2020-05-15 23:08:22 +0800

    Update insert.js

--- inc/insert.js
+++ inc/insert.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	if(typeof loaded == "undefined") {
 		loaded = true;
-		document.querySelectorAll('body')[0].insertAdjacentHTML('beforeend', '<style>#eqipimage{display:none;background:#FFF;position:fixed;top:0;left:0;width:100%;height:100%;z-index:1000}#eqipimage:target{display:block}#eqipimage img{max-width:90%;max-height:80%;margin-top:2%}</style><div id="eqipimage"><a href="#!" id="close" style="font-size:40px;cursor:pointer;position:fixed;right:30px;top:0;text-decoration:none;color:black">&times;</a><img></div>');
+		document.querySelectorAll('body')[0].insertAdjacentHTML('beforeend', '<style>#eqipimage{display:none;background:#FFF;position:fixed;top:0;left:0;width:100%;height:100%;z-index:1000;text-align:center}#eqipimage:target{display:block}#eqipimage img{max-width:90%;max-height:80%;margin-top:2%}</style><div id="eqipimage"><a href="#!" id="close" style="font-size:40px;cursor:pointer;position:fixed;right:30px;top:0;text-decoration:none;color:black">&times;</a><img></div>');
 		imgList = {
 			"md43": "https://i.imgur.com/Byr01yh.jpg"
 		}
