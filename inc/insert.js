72c521ad986738ae753a642374f7097f33afb5ed
Henry Wu <its@henrywu.tw>
2020-05-15 20:31:18 +0800

    Update insert.js

--- inc/insert.js
+++ inc/insert.js
@@ -1,11 +1,11 @@
 imgList = {
-  "md43": "https://i.imgur.com/Byr01yh.jpg"
+	"md43": "https://i.imgur.com/Byr01yh.jpg"
 }
 eqipTable = document.querySelectorAll('span[name=table1] tr');
 for(i = 2; i <= eqipTable.length; i++) {
-  if(imgList[document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim()] != undefined) {
-    document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td:nth-child(3) span')[0].onclick = function() {
-		console.log(imgList[document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim()]);
+	if(imgList[document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim()] != undefined) {
+		document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td:nth-child(3) span')[0].onclick = function() {
+			console.log(imgList[document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim()]);
+		}
 	}
-  }
 }
