f937f2d3ab5285de028b03b3e4e04a7e55519af6
Henry Wu <its@henrywu.tw>
2020-05-15 20:29:42 +0800

    Update insert.js

--- inc/insert.js
+++ inc/insert.js
@@ -1,8 +1,11 @@
-console.log('Hello!');
 imgList = {
-  "T01": "https://i.imgur.com/Byr01yh.jpg"
+  "md43": "https://i.imgur.com/Byr01yh.jpg"
 }
 eqipTable = document.querySelectorAll('span[name=table1] tr');
 for(i = 2; i <= eqipTable.length; i++) {
-  console.log(imgList[document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim()]);
+  if(imgList[document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim()] != undefined) {
+    document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td:nth-child(3) span')[0].onclick = function() {
+		console.log(imgList[document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim()]);
+	}
+  }
 }
