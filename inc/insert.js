fa088aa3014fa6b497e7b7f52f6b05a516a366fc
Henry Wu <its@henrywu.tw>
2020-05-15 21:28:16 +0800

    Update insert.js

--- inc/insert.js
+++ inc/insert.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
+	document.write('<style>#eqipimage{display:none;background:#FFF;position:fixed;top:0;left:0;width:100%;height:100%;}#eqipimage:target{display:block}</style><div id="eqipimage"></div>');
 	imgList = {
 		"md43": "https://i.imgur.com/Byr01yh.jpg"
 	}
