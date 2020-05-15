31138f9723845a88882dc962949c3cf17286f9b9
Henry Wu <its@henrywu.tw>
2020-05-15 23:33:34 +0800

    Update insert.js

--- inc/insert.js
+++ inc/insert.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		eqipTable = document.querySelectorAll('span[name=table1] tr');
 		for(i = 2; i <= eqipTable.length; i++) {
 			img = imgList[document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim()];
+			console.log(img);
 			if(img != undefined) {
 				document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td:nth-child(3) span')[0].onclick = function() {
 					location.href = '#eqipimage';
