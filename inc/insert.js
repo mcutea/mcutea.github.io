8a1ab3e7b056bd0f419f87c556489ba1d38a6cfd
Henry Wu <its@henrywu.tw>
2020-05-15 20:37:12 +0800

    Update insert.js

--- inc/insert.js
+++ inc/insert.js
@@ -1,12 +1,14 @@
-imgList = {
-	"md43": "https://i.imgur.com/Byr01yh.jpg"
-}
-eqipTable = document.querySelectorAll('span[name=table1] tr');
-for(i = 2; i <= eqipTable.length; i++) {
-	img = imgList[document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim()];
-	if(img != undefined) {
-		document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td:nth-child(3) span')[0].onclick = function() {
-			console.log(img);
+document.addEventListener("DOMContentLoaded", function(event) { 
+	imgList = {
+		"md43": "https://i.imgur.com/Byr01yh.jpg"
+	}
+	eqipTable = document.querySelectorAll('span[name=table1] tr');
+	for(i = 2; i <= eqipTable.length; i++) {
+		img = imgList[document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim()];
+		if(img != undefined) {
+			document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td:nth-child(3) span')[0].onclick = function() {
+				console.log(img);
+			}
 		}
 	}
-}
+});
