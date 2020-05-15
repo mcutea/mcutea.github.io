d187cc6b747de42bd78cebc5f28a86a011a5b061
Henry Wu <its@henrywu.tw>
2020-05-15 21:36:20 +0800

    Update insert.js

--- inc/insert.js
+++ inc/insert.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-	document.getElementById('body').innerHTML += '<style>#eqipimage{display:none;background:#FFF;position:fixed;top:0;left:0;width:100%;height:100%;z-index:1000}#eqipimage:target{display:block}</style><div id="eqipimage"><a href="#!" id="close" style="font-size:40px;cursor:pointer;position:fixed;right:30px;top:0;text-decoration:none;color:black">&times;</a></div>';
+	document.getElementById('text11').innerHTML += '<style>#eqipimage{display:none;background:#FFF;position:fixed;top:0;left:0;width:100%;height:100%;z-index:1000}#eqipimage:target{display:block}</style><div id="eqipimage"><a href="#!" id="close" style="font-size:40px;cursor:pointer;position:fixed;right:30px;top:0;text-decoration:none;color:black">&times;</a></div>';
 	imgList = {
 		"md43": "https://i.imgur.com/Byr01yh.jpg"
 	}
