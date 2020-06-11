4251d7380c8b754643a87fc83c9f7d3e5b9f3a86
Henry Wu <its@henrywu.tw>
2020-06-11 18:34:41 +0800

    Update insert.js

--- inc/insert.js
+++ inc/insert.js
@@ -16,17 +16,19 @@ var showimg = function(id) {
 }
 document.addEventListener("DOMContentLoaded", function(event) {
 	if(typeof loaded == "undefined") {
-		loaded = true;
-		location.href = '#!';
-		document.querySelectorAll('body')[0].insertAdjacentHTML('beforeend', '<style>#eqipimage{display:none;background:#FFF;position:fixed;top:0;left:0;width:100%;height:100%;z-index:1000;text-align:center}#eqipimage:target{display:block}#eqipimage img{max-width:70%!important;max-height:70%!important;margin-top:5%}</style><div id="eqipimage"><a href="#!" id="close" style="font-size:40px;cursor:pointer;position:fixed;right:10px;top:0;text-decoration:none;color:black">&times;</a><img></div>');
-		eqipTable = document.querySelectorAll('span[name=table1] tr');
-		var id;
-		for(var i = 2; i <= eqipTable.length; i++) {
-			id = document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim();
-			if(imgList[id] != undefined) document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td:nth-child(3) span')[0].setAttribute('onclick','showimg("'+id+'")');
-			else {
-				document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td:nth-child(3) span')[0].removeAttribute('onclick')
-				document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td:nth-child(3) span')[0].removeAttribute('style')
+		if(document.querySelectorAll('input')[0].value == '器材借用申請') {
+			loaded = true;
+			location.href = '#!';
+			document.querySelectorAll('body')[0].insertAdjacentHTML('beforeend', '<style>#eqipimage{display:none;background:#FFF;position:fixed;top:0;left:0;width:100%;height:100%;z-index:1000;text-align:center}#eqipimage:target{display:block}#eqipimage img{max-width:70%!important;max-height:70%!important;margin-top:5%}</style><div id="eqipimage"><a href="#!" id="close" style="font-size:40px;cursor:pointer;position:fixed;right:10px;top:0;text-decoration:none;color:black">&times;</a><img></div>');
+			eqipTable = document.querySelectorAll('span[name=table1] tr');
+			var id;
+			for(var i = 2; i <= eqipTable.length; i++) {
+				id = document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim();
+				if(imgList[id] != undefined) document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td:nth-child(3) span')[0].setAttribute('onclick','showimg("'+id+'")');
+				else {
+					document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td:nth-child(3) span')[0].removeAttribute('onclick')
+					document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td:nth-child(3) span')[0].removeAttribute('style')
+				}
 			}
 		}
 	}
