e8666a7b2fa901dd7fa093db1e89a067fad6d869
Henry Wu <its@henrywu.tw>
2020-06-11 18:35:52 +0800

    Update insert.js

--- inc/insert.js
+++ inc/insert.js
@@ -17,6 +17,7 @@ var showimg = function(id) {
 document.addEventListener("DOMContentLoaded", function(event) {
 	if(typeof loaded == "undefined") {
 		if(document.querySelectorAll('input')[0].value == '器材借用申請') {
+			document.querySelectorAll('#field8')[0].setAttribute('placeholder','請填寫活動場地');
 			loaded = true;
 			location.href = '#!';
 			document.querySelectorAll('body')[0].insertAdjacentHTML('beforeend', '<style>#eqipimage{display:none;background:#FFF;position:fixed;top:0;left:0;width:100%;height:100%;z-index:1000;text-align:center}#eqipimage:target{display:block}#eqipimage img{max-width:70%!important;max-height:70%!important;margin-top:5%}</style><div id="eqipimage"><a href="#!" id="close" style="font-size:40px;cursor:pointer;position:fixed;right:10px;top:0;text-decoration:none;color:black">&times;</a><img></div>');
