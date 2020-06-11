c4e9ae5a6198782ca31f5192e253c6e8fd27fb33
Henry Wu <its@henrywu.tw>
2020-06-12 00:05:30 +0800

    Update i.js

--- i.js
+++ i.js
@@ -36,6 +36,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			}
 		}
 		if(document.querySelectorAll('input')[0].value == '個人借用紀錄') {
+			document.querySelectorAll('button')[0].style.display = 'none';
 			if(document.querySelectorAll('input')[18].value == '') document.querySelectorAll('button')[2].style.display = 'none';
 			eqipTable = document.querySelectorAll('span[name=table1] tr');
 			var id;
