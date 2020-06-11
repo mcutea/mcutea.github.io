9c8970d17d2df73c0d35901e02792934aa1a7d16
Henry Wu <its@henrywu.tw>
2020-06-12 00:33:28 +0800

    Update i.js

--- i.js
+++ i.js
@@ -49,15 +49,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			eqipTable = document.querySelectorAll('span[name=table1] tr');
 			var id;
 			for(var i = 2; i <= eqipTable.length; i++) {
-				id = document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[1].innerText.trim();
-				id2 = document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[2].innerText.trim();
-				if(id == 'zhide' || id2 == 'zhide') document.querySelectorAll('span[name=table1] tr:nth-child('+i+')')[0].style.display = 'none';
+				id = document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim();
+				if(id == 'zhide') document.querySelectorAll('span[name=table1] tr:nth-child('+i+')')[0].style.display = 'none';
 			}
 			eqipTable2 = document.querySelectorAll('span[name=table2] tr');
 			for(var i = 2; i <= eqipTable.length; i++) {
 				id = document.querySelectorAll('span[name=table2] tr:nth-child('+i+') td')[1].innerText.trim();
-				id2 = document.querySelectorAll('span[name=table2] tr:nth-child('+i+') td')[2].innerText.trim();
-				if(id == 'zhide' || id2 == 'zhide') document.querySelectorAll('span[name=table2] tr:nth-child('+i+')')[0].style.display = 'none';
+				if(id == 'zhide') document.querySelectorAll('span[name=table2] tr:nth-child('+i+')')[0].style.display = 'none';
 			}
 		}
 	}
