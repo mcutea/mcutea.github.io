f52cfe528fbdf72f3c386d92653453172469cf58
Henry Wu <its@henrywu.tw>
2020-06-15 17:16:54 +0800

    Update i.js

--- i.js
+++ i.js
@@ -87,7 +87,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				if(id == 'zhide') document.querySelectorAll('span[name=table1] tr:nth-child('+i+')')[0].style.display = 'none';
 			}
 			eqipTable2 = document.querySelectorAll('span[name=table2] tr');
-			for(var i = 2; i <= eqipTable.length; i++) {
+			for(var i = 2; i <= eqipTable2.length; i++) {
 				id = document.querySelectorAll('span[name=table2] tr:nth-child('+i+') td')[1].innerText.trim();
 				if(id == 'zhide') document.querySelectorAll('span[name=table2] tr:nth-child('+i+')')[0].style.display = 'none';
 			}
