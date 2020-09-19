efcfaa79557a0c0aefdefa6a2351327674d5aa7d
Henry Wu <its@henrywu.tw>
2020-09-19 22:59:39 +0800

    Update i.js

--- i.js
+++ i.js
@@ -1,4 +1,4 @@
-var imgList = {"id":"image"}
+var imgList = {"A01":"KmAsVez.jpg","A02":"xZyyEwW.jpg","A03":"v1jaUt4.png","A04":"C5Rvk4S.png","A05":"e8xjYx4.png","A06":"7krlQGq.png","A07":"2PvvMEKh.png","A08":"sJM4kLL.png","A09":"aZ5v6Em.png","A10":"JoO25MV.jpg","A11":"x3HuSj2.png","A12":"yDfVbux.jpg","A13":"4NQRPDFh.jpg","A14":"W8vGiyZ.png","A15":"ONSYmOi.png","A16":"me7ym0U.png","A17":"zeRaypM.png","B01":"x0FBm3I.jpg","B02":"fZmurQV.jpg","B03":"FucGNn9.jpg","B04":"lsQWCdH.png","B05":"td9W0vD.jpg","B06":"J43J5aQ.jpg","B07":"bMebY8N.jpg","B08":"qtKaejY.jpg","B09":"5P7awa3.jpg","B10":"XcW3xnW.jpg","C01":"hCgP48eh.png","C02":"YHhSqIW.jpg","C03":"ambwzb7h.jpg","C04":"qOaZt0z.png","C05":"kZfo8Ox.png","D01":"dXK2Sf5h.jpg","D02":"cquBncI.jpg","D03":"uBDToWM.jpg","D04":"auxbELf.jpg","D05":"nAjYk7W.jpg","D06":"pq7uQU5.jpg","E01":"3fPEba6.jpg","E02":"YGumo7c.jpg","E03":"V1aR9EO.jpg","E04":"hssjwT3.jpg","E05":"O5RKfK0.jpg","E06":"FhYslqe.jpg","E07":"UqU9sHI.jpg","E09":"BMA0Gwi.jpg","E10":"d7PLmrG.jpg","E11":"BMSPFOx.jpg","E12":"OOYndR7.jpg","E13":"xQG2KQz.jpg","E14":"ZaGuIDV.jpg","E15":"1mYGRBC.jpg","E16":"DEIia1R.jpg","E17":"I3b6p3o.jpg","E18":"Ydrg1Q7.jpg","E19":"etfgrE7.jpg","E20":"4JWaIXT.jpg","E21":"4HOBibR.jpg","E22":"U1hFgTC.jpg","E23":"1YFkGlO.jpg","E24":"OLExBu2.jpg","F01":"Xtrbo7z.png","F02":"nvz5YZY.png","F03":"0kK1RXc.png","F04":"3HmaDYt.jpg","F05":"XQZRBwq.png","F08":"rZ4tmLL.jpg","F09":"WNXu73B.png","F13":"ozRfPUM.png"}
 var showimg = function(id) {
 	location.href ='#eqipimage';
 	document.querySelectorAll('#eqipimage img')[0].src = '';
@@ -6,13 +6,13 @@ var showimg = function(id) {
 	imgele = document.querySelectorAll('#eqipimage img')[0];
 	imgele.removeAttribute('width')
 	imgele.removeAttribute('height');
-	imgele.src = 'http://emaker.mcut.edu.tw/servlet/jform?step=2&file=borrow.dat&enc='+imgList[id];
+	imgele.src = 'https://i.imgur.com/'+imgList[id];
 	img.onload = function() {
 		outele = document.querySelectorAll('#eqipimage')[0];
 		if(this.height < this.width) imgele.width = outele.offsetWidth*0.8;
 		else imgele.height = outele.offsetHeight*0.8;
 	}
-	img.src = 'http://emaker.mcut.edu.tw/servlet/jform?step=2&file=borrow.dat&enc='+imgList[id];
+	img.src = 'https://i.imgur.com/'+imgList[id];
 }
 document.addEventListener("DOMContentLoaded", function(event) {
 	if(typeof loaded == "undefined") {
@@ -38,61 +38,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				}
 			}
 		}
-		/*
-		if(document.querySelectorAll('input')[0].value == '個人借用紀錄') {
-			document.querySelectorAll('button')[0].style.display = 'none';
-			if(document.querySelectorAll('input')[18].value == '') {
-				document.querySelectorAll('button')[2].style.display = 'none';
-				document.querySelectorAll('span[name=table1]')[0].style.display = 'none';
-			} else {
-				if(localStorage.canShowPrint) {
-					var xhr = new XMLHttpRequest();
-					xhr.addEventListener('load', function() {
-						d1 = document.querySelectorAll('td')[6].innerText.trim();
-						d2 = document.querySelectorAll('td')[7].innerText.trim();
-						parsedDate = d1.substr(0,4)+'/'+d1.substr(4,2)+'/'+d1.substr(6,2);
-						parsedDate += ' ~ ' + d2.substr(0,4)+'/'+d1.substr(4,2)+'/'+d1.substr(6,2);
-						document.querySelectorAll('body')[0].insertAdjacentHTML('beforeend', this.responseText);
-						document.querySelectorAll('#print_field_13')[0].innerText = document.querySelectorAll('input')[13].value;
-						document.querySelectorAll('#print_field_14')[0].innerText = document.querySelectorAll('input')[14].value;
-						document.querySelectorAll('#print_field_15')[0].innerText = document.querySelectorAll('input')[15].value;
-						document.querySelectorAll('#print_field_16')[0].innerText = document.querySelectorAll('input')[16].value;
-						document.querySelectorAll('#print_field_17')[0].innerText = document.querySelectorAll('input')[17].value.split(' ')[1];
-						document.querySelectorAll('#print_field_18')[0].innerText = document.querySelectorAll('input')[18].value;
-						document.querySelectorAll('#print_field_19')[0].innerText = document.querySelectorAll('input')[19].value;
-						document.querySelectorAll('#print_field_date')[0].innerText = parsedDate;
-						eqipTable2 = document.querySelectorAll('span[name=table1] tr');
-						for(var j = 2; j <= eqipTable2.length; j++) {
-							if(document.querySelectorAll('span[name=table1] tr:nth-child('+j+') td')[1].innerText.trim() != 'zhide') {
-								table_html = '<tr><td>'+document.querySelectorAll('span[name=table1] tr:nth-child('+j+') td')[2].innerText.trim()+'</td><td>'+document.querySelectorAll('span[name=table1] tr:nth-child('+j+') td')[3].innerText.trim()+'</td><td>'+document.querySelectorAll('span[name=table1] tr:nth-child('+j+') td')[4].innerText.trim()+'</td><td style="text-align:right">'+document.querySelectorAll('span[name=table1] tr:nth-child('+j+') td')[5].innerText.trim()+'</td></tr>';
-								document.querySelectorAll('#print_form_borrow tbody')[0].insertAdjacentHTML('beforeend', table_html);
-							}
-						}
-					});
-					xhr.open('GET', 'https://mcutea.github.io/print.html');
-					xhr.send(null);
-				}
-			}
-			eqipTable = document.querySelectorAll('span[name=table1] tr');
-			var id;
-			for(var i = 2; i <= eqipTable.length; i++) {
-				id = document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[1].innerText.trim();
-				if(id == 'zhide') document.querySelectorAll('span[name=table1] tr:nth-child('+i+')')[0].style.display = 'none';
-			}
-		}
-		if(document.querySelectorAll('input')[0].value == '課外組管理員') {
-			eqipTable = document.querySelectorAll('span[name=table1] tr');
-			var id;
-			for(var i = 2; i <= eqipTable.length; i++) {
-				id = document.querySelectorAll('span[name=table1] tr:nth-child('+i+') td')[0].innerText.trim();
-				if(id == 'zhide') document.querySelectorAll('span[name=table1] tr:nth-child('+i+')')[0].style.display = 'none';
-			}
-			eqipTable2 = document.querySelectorAll('span[name=table2] tr');
-			for(var i = 2; i <= eqipTable2.length; i++) {
-				id = document.querySelectorAll('span[name=table2] tr:nth-child('+i+') td')[1].innerText.trim();
-				if(id == 'zhide') document.querySelectorAll('span[name=table2] tr:nth-child('+i+')')[0].style.display = 'none';
-			}
-		}
-		*/
 	}
 });
