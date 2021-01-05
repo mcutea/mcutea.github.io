2d9585d63d74efa617cec1eb8062d93685770499
Henry Wu <its@henrywu.tw>
2021-01-05 13:32:12 +0800

    Update i.js

--- i.js
+++ i.js
@@ -1,1 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+if(document.querySelectorAll('input')[0].value == '器材借用申請' || document.querySelectorAll('input')[0].value == '個人借用紀錄') {
 location.href = 'https://info.mcut.edu.tw/web1/borrow/login.aspx';
+}
+});
